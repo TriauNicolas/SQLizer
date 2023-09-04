734b6d304aebac5ed13fd66c185f5a8b3546eca7
Triau Nicolas <n_triau@hetic.eu>
2023-09-04 12:08:11 +0200

    Fix: Wrong architecture for NextJS

---
+++ src/pages/test.tsx
@@ -0,0 +1,24 @@
+// 'use client'
+
+// import { useState } from 'react'
+// import styles from '../styles/page.module.css'
+// import { CanvasElement } from '../components/CanvasElemment/CanvasElement'
+// import { PredefinedTable } from '../components/PredefinedTable/PredefinedTable'
+
+// export default function Home() {
+//   const [elementToDraw, setElementToDraw] = useState('')
+
+//   const handlerForCallbackSelection = (selectedElement: string) => {
+//     setElementToDraw(selectedElement)
+//   }
+
+//   return (
+//     <div className={styles.mainDiv}>
+//       <h1>Canvas Test</h1>
+//       <div className={styles.containerCanvas}>
+//         <PredefinedTable callbackSelection={handlerForCallbackSelection}/>
+//         <CanvasElement elementToDraw={elementToDraw}/>
+//       </div>
+//     </div>
+//   )
+// }
