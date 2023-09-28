9c0f2307deb2af119fe9a6c36e4f33562c487b23
valentinsejourne <v_sejourne@hetic.eu>
2023-09-28 05:52:03 +0200

    fix: folders to create a router

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { CanvasElement } from '@/components/Canvas/CanvasElement'
+import Layout from './layout'
+
+
+export default function Home() {
+
+  return (
+      <Layout>
+        <CanvasElement />
+      </Layout>
+  )
+}
