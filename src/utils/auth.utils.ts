8c616d504e08ff808645b0235ec9f12c9a5c777a
gabrielseron <gabriel.seron.pro@gmail.com>
2023-09-29 20:22:28 +0200

    Feat: Add token utils / verifs

---
+++ src/utils/auth.utils.ts
@@ -0,0 +1,23 @@
+import { getAxiosInstance } from "@/api/axios";
+
+export const setToken = (token: string) => {
+    localStorage.setItem('token', token)
+}
+
+export const getToken = (): string | null => {
+    const token = localStorage.getItem('token')
+    return token || null;
+}
+
+export const isUserLogged = async (): Promise<boolean> => {
+    try {
+        const res = await getAxiosInstance().get('/auth/verifToken')
+        console.log(res);
+        return true;
+    } catch (error) {
+        console.log('error: ');
+        console.log(error);
+    }
+
+    return false;
+}
