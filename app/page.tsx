07db33f07dc3bf19a56da9ab9deae59c55632acf
gabrielseron <gabriel.seron.pro@gmail.com>
2023-10-04 15:52:45 +0200

    fix: remove use client and unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { CanvasElement } from '@/components/CanvasComponents/Canvas/CanvasElement'
 import Layout from './layout'
-import { useSession } from 'next-auth/react';
 
 export default function Home() {
   return (
