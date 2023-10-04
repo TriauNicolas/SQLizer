c494582183826a276eef5660fac0f413c47f1401
gabrielseron <gabriel.seron.pro@gmail.com>
2023-10-04 14:57:11 +0200

    Feature: Implement next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { CanvasElement } from '@/components/CanvasComponents/Canvas/CanvasElement'
 import Layout from './layout'
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
   return (
