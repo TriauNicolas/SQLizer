7c44dbe0d681ff5b68bebb23b46dd351d0afdedc
gabrielseron <gabriel.seron.pro@gmail.com>
2023-10-04 15:53:15 +0200

    fix: indentation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -22,7 +22,7 @@ const Login = () => {
         password: data.password,
         redirect: false
       });
-        if (response?.ok) {
+      if (response?.ok) {
         router.push("/")
       }
 
