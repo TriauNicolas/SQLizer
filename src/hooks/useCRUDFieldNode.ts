e7558b92bfc6362804092bb5b0d05d29e485b311
Triau Nicolas <n_triau@hetic.eu>
2023-10-13 18:38:12 +0200

    Add: Update fields socket

--- src/hooks/useCRUDFieldNode.ts
+++ src/hooks/useCRUDFieldNode.ts
@@ -17,7 +17,7 @@ export const useCRUDFieldNode = (setNodes: Function, setEdges: Function) => {
       const offsetY = 50 + (40 * numberFieldsInParent);
   
       // Prepare node object
-      const newField = { 
+      const newField = {
         id: (allNodes.length + 1).toString(),
         type: "fieldNode", 
         position: { x: 0, y: offsetY },
@@ -27,8 +27,8 @@ export const useCRUDFieldNode = (setNodes: Function, setEdges: Function) => {
         selected: false,
         hidden: false
       }
-
-      // // Update the nodes
+      
+      // Update the nodes
       setNodes([...allNodes, newField]);
 
       // Update height parent
@@ -38,5 +38,20 @@ export const useCRUDFieldNode = (setNodes: Function, setEdges: Function) => {
     }
   }
 
-  return { sendSocketField, addField }
+  const updateField = (tableName: string, fieldName: string, data: Field) => {
+    console.log({ tableName })
+    console.log({ fieldName })
+    console.log({ data })
+    const allNodes = getNodes();
+
+    // Update the fieldNode
+    setNodes(allNodes.map((node) => {
+      if (node.data.title === `${tableName}.${fieldName}`) {
+        return ({...node, data: data})
+      }
+      return ({...node})
+    }));
+  }
+
+  return { sendSocketField, addField, updateField }
 }
