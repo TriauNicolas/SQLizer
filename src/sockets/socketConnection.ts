5002f9226642a8071938dbfea07ea8abadd8d2f8
gabrielseron <gabriel.seron.pro@gmail.com>
2023-10-15 16:59:32 +0200

    Feat: Add connexion requirement for canvas page

--- src/sockets/socketConnection.ts
+++ src/sockets/socketConnection.ts
@@ -1,14 +1,12 @@
 import { io, Socket } from "socket.io-client";
 // import { getToken } from '@/utils/auth.utils';
 
-export const socketConnection = () => {
+export const socketConnection = (token: string, roomId: string) => {
   console.log("Start connection")
   
   const url = process.env.API_URL
   if (!url) throw new Error("url is not defined");
-  
-  const roomId = "32b4a4a6-1f98-4862-932b-059f0b72f06c";
-  
+
   const socket: Socket = io(url, {
     path: "/sqlizer/",
     query: {
@@ -17,12 +15,12 @@ export const socketConnection = () => {
     transportOptions: {
       polling: {
         extraHeaders: {
-          BearerToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFhOGRmNzU5LTA5ZDQtNGFiMy05Y2U1LTFjNjBiZDRlNGRiOSIsImlhdCI6MTY5NzMwNzAzMywiZXhwIjoxNjk3OTExODMzfQ.O-vuqIzEvjcVtVFo0FGd8dvOtksCmLc8_4AzrEImJys'
+          BearerToken: token
         }
       }
     }
   });
-  
+
   socket?.on('socketError', (data: any) => console.log(data));
   console.log("Connection successful")
 
